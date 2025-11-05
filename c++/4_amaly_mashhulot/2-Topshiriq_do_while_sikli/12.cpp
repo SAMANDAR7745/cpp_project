e792cef288b92b6306058c985eab6a4d31a337e1
SAMANDAR7745 <samandar7745@gmail.com>
2025-11-05 13:16:34 +0500

    Yangilandi: 2-tpshoirq 12-misol

--- c++/4_amaly_mashhulot/2-Topshiriq_do_while_sikli/12.cpp
+++ c++/4_amaly_mashhulot/2-Topshiriq_do_while_sikli/12.cpp
@@ -3,4 +3,13 @@
 using namespace std;
 
 int main(){
+    double a, i = 1 ;
+    cout << "Necha hello kerak:";
+    cin >> a;
+    do
+    {
+        cout << i << "."<< "Hello World" << endl;
+        i ++;
+    } while (i<=a);
+    
 }
